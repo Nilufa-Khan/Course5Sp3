55bea9db53d9dddd4a2524d7b679ca613e696b12
Nilufa Khan <khannilufa5@gmail.com>
2022-09-04 13:17:13 +0530

    implement the compare method inside the class

--- src/NameComparator.java
+++ src/NameComparator.java
@@ -1,5 +1,7 @@
 import java.util.Comparator;
 
 public class NameComparator implements Comparator<Book> {
-    
+    public int compare(Book b1 , Book b2){
+        return  b1.getAuthor().compareTo(b2.getAuthor());
+    }
 }
