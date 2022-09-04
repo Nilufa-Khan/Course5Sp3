4900fef63741f4a0e0faa0ac6e856253d3c7814b
Nilufa Khan <khannilufa5@gmail.com>
2022-09-04 13:18:04 +0530

    creating a student class

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Student {
+    public static void main(String[] args) {
+        //implementing hashset
+        //set is a reference variable and hashset is a implementation class
+        //set produce unorderd and unique value
+        Set<Integer> integerSet = new HashSet<>();
+
+        }
+    }
