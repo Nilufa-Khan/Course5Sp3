291d2ce845fac773cad1a21e4963057f941a4814
Nilufa Khan <khannilufa5@gmail.com>
2022-09-06 14:30:02 +0530

    creating 4 private attributes

--- src/Book.java
+++ src/Book.java
@@ -4,6 +4,7 @@ public class Book implements Comparable<Book>{
     private String author;
     private double price;
     private long isbn;
+
     public Book() {
     }
     public Book(String bookName, String author, double price, long isbn) {
